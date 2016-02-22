843b35d3e427c1d8d0765ac77e3e5bdd58e4934a
Jérôme Avoustin <jerome@rehia.fr>
2016-02-22 23:51:54 +0100

    c# initialisation

--- javascript/src/UserSession.js
+++
@@ -1,8 +0,0 @@
-class UserSession {
-    getLoggedUser() {
-        throw new Error("UserSession.getLoggedUser() should not be called in an unit test");
-    }
-
-}
-
-export default new UserSession();
